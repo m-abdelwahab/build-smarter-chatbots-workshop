7c02d57a09cdfa9f389c85ccc6f3f9d64278942c
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-07-12 13:39:08 +0400

    Add solution code

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,14 @@
 // app/api/chat/route.ts
+import { mistral } from "@ai-sdk/mistral";
+import { streamText } from "ai";
+
 export async function POST(req: Request) {
 	const { messages } = await req.json();
 
-	return new Response("Chat messages received!");
+	const result = await streamText({
+		model: mistral("open-mistral-7b"),
+		messages,
+	});
+
+	return result.toAIStreamResponse();
 }
